c88b29f0825f515030c5907cec9abfd148e239bc
ShanYi-Hui <shanyi.hui@qq.com>
2025-04-22 15:08:58 +0800

    引入 Ant Design 组件库，添加 Antd5 与 React19 的兼容处理

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from 'antd';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button type="primary">Button</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
