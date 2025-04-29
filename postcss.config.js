0fb2702cad7c4ff3dd9872b5f95ba77b63aec2f2
ShanYi-Hui <shanyi.hui@qq.com>
2025-04-29 14:33:11 +0800

    修复 postcss 配置符合 ESM 规范

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
-module.exports = {
+// postcss.config.mjs
+
+import postcssNesting from 'postcss-nesting';
+import postcssPresetEnv from 'postcss-preset-env';
+
+export default {
   plugins: [
     // 官方推荐的嵌套语法插件，支持CSS嵌套
-    require('postcss-nesting')(),
+    postcssNesting(),
 
     // 启用现代CSS特性，并自动降级以兼容目标浏览器
-    require('postcss-preset-env')({
+    postcssPresetEnv({
       stage: 3, // 启用第3阶段的CSS特性（尝鲜高效又稳定，美滋滋）。stage 0：初步提案，stage 1：获得关注，正在探索，stage 2：草案，可能会有实现，stage 3：候选推荐，准备标准化，stage 4：正式标准
       features: {
         'nesting-rules': false, // 禁用嵌套规则，避免与postcss-nesting冲突
